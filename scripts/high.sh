f8c8726b342937cc9d6ee9bae61ab699bd5b88a4
Zach Kaylor <avanasear@gmail.com>
2019-09-25 14:04:05 -0400

    basic additions

--- scripts/high.sh
+++ scripts/high.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # deployment script for ec2 - high variant
+
+# UPDATE INSTANCE
+yum update -y
+
+# INSTALL NECESSARY ADDITIONS
+yum install python3 -y
+
+# INSTALL NICE-TO-HAVES
+yum install tmux -y
+yum install vim -y
--- scripts/medium.sh
+++ scripts/medium.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # deployment script for ec2 - medium variant
+
+# UPDATE INSTANCE
+yum update -y
+
+# INSTALL NECESSARY ADDITIONS
+yum install python3 -y
+
+# INSTALL NICE-TO-HAVES
+yum install tmux -y
+yum install vim -y

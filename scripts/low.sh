b6b718671e10e2b9fd7df54be0a7e3bc784bd6f8
Zach Kaylor <avanasear@gmail.com>
2019-09-25 14:31:05 -0400

    added -y to EPEL

--- scripts/low.sh
+++ scripts/low.sh
@@ -10,6 +10,6 @@ yum install python3 -y
 # INSTALL NICE-TO-HAVES
 yum install tmux -y
 yum install vim -y
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
 subscription-manager repos --enable "codeready-builder-for-rhel-8-*-rpms"
 yum install htop -y

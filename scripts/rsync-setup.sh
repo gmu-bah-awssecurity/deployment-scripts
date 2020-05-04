8b8143a931e4104fe5028d13fe9bdd005a4a09d6
avanasear <avanasear@gmail.com>
2020-05-03 23:35:37 -0400

    fixed errors and updated functionality

--- scripts/rsync-setup.sh
+++ scripts/rsync-setup.sh
@@ -1,22 +1,45 @@
 #!/usr/bin/env bash
 
-IDENTITY=$1
+RSYNC_START="rsync -a -zz -e ssh"
+RSYNC_DEST="/var/instance-logs"
 
-if [ "$#" -gt 2 ]; then
-    PUBKEY=$(ssh -i $IDENTITY ubuntu@$2 'sudo cat /root/.ssh/id_rsa.pub')
-else
-    echo "Not enough arguments"
-    echo "Usage:"
-    echo "rsync-setup.sh <ssh identity> <management instance ip> [list of ip addresses to manage]"
-    exit
-fi
-
-shift
-shift
-
-for ip in "$@"; do
-    ssh -i $IDENTITY ubuntu@$1 "echo $PUBKEY | sudo tee -a /root/.ssh/authorized_keys"
-    (crontab -l 2>/dev/null; echo "") | crontab -
-    shift
+while getopts "i:f:l:m:h:" arg; do
+    case $arg in
+        i)
+            ID=$OPTARG
+            ;;
+        f)
+            MANAGEMENT=$OPTARG
+            PUBKEY=$(ssh -i $ID ubuntu@$OPTARG 'sudo cat /root/.ssh/id_rsa.pub')
+            ;;
+        l)
+            ssh -i $ID ubuntu@$MANAGEMENT "sudo mkdir -p $RSYNC_DEST/$OPTARG && sudo ssh-keyscan -H $OPTARG >> ~/.ssh/known_hosts"
+            ssh -i $ID ubuntu@$OPTARG "echo $PUBKEY | sudo tee -a /root/.ssh/authorized_keys"
+            ssh -i $ID ubuntu@$MANAGEMENT "echo 0 */2 \* \* \* $RSYNC_START $OPTARG:/var/log $RSYNC_DEST/$OPTARG | sudo tee -a /var/spool/cron/crontabs/root"
+            ;;
+        m)
+            ssh -i $ID ubuntu@$MANAGEMENT "sudo mkdir -p $RSYNC_DEST/$OPTARG && sudo ssh-keyscan -H $OPTARG >> ~/.ssh/known_hosts"
+            ssh -i $ID ubuntu@$OPTARG "echo $PUBKEY | sudo tee -a /root/.ssh/authorized_keys"
+            ssh -i $ID ubuntu@$MANAGEMENT "echo 0 \* \* \* \* $RSYNC_START $OPTARG:/var/log $RSYNC_DEST/$OPTARG | sudo tee -a /var/spool/cron/crontabs/root"
+            ;;
+        h)
+            ssh -i $ID ubuntu@$MANAGEMENT "sudo mkdir -p $RSYNC_DEST/$OPTARG && sudo ssh-keyscan -H $OPTARG >> ~/.ssh/known_hosts"
+            ssh -i $ID ubuntu@$OPTARG "echo $PUBKEY | sudo tee -a /root/.ssh/authorized_keys"
+            ssh -i $ID ubuntu@$MANAGEMENT "echo */15 \* \* \* \* $RSYNC_START $OPTARG:/var/log $RSYNC_DEST/$OPTARG | sudo tee -a /var/spool/cron/crontabs/root"
+            ;;
+        \?)
+            echo "Usage:"
+            echo "rsync-setup.sh"
+            echo "    -i <ssh identity>"
+            echo "    -f <management instance ip or hostname>"
+            echo ""
+            echo "    -l <low-security instance ip or hostname>"
+            echo "    -m <med-security instance ip or hostname>"
+            echo "    -h <high-security instance ip or hostname>"
+            echo ""
+            echo "Make sure you enter each low/med/high security instance with its own '-l', '-m', or '-h'."
+            exit
+            ;;
+    esac
 done
 
